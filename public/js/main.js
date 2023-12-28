eda1e6895e3918a82cedcfadeefa49fc39b8f92e
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2023-12-28 17:22:49 +0530

    go: responsive & validation code updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 import JustValidate from "just-validate";
 
-const formEl = document.getElementById("mainform");
+const formEl = document.querySelector("#mainform");
 
 const validator = new JustValidate(formEl, {
   validateBeforeSubmitting: true,
@@ -135,19 +135,118 @@ validator.addField("#companyName", [
   },
 ]);
 
-validator.addField("#companyLogo", [
-  {
-    rule: "minFilesCount",
-    value: 1,
-    errorMessage: "company logo is required",
-  },
-  {
-    rule: "maxFilesCount",
-    value: 1,
-  },
-]);
+// validator.addField("#companyLogo", [
+//   {
+//     rule: "minFilesCount",
+//     value: 1,
+//     errorMessage: "company logo is required",
+//   },
+//   {
+//     rule: "maxFilesCount",
+//     value: 1,
+//   },
+// ]);
 
-validator.onSuccess(() => {
-  const formData = new FormData(formEl);
-  console.log(formData);
+// preview main section
+
+// I am not using FORM for Preview Section so i write code on my own
+const previewSection = document.querySelector("#previewSection");
+
+// loads (i know this part kinda messsi 😅)
+const previewAgencyName = document.querySelector("#previewAgencyName");
+const previewAgencyType = document.querySelector("#previewAgencyType");
+const previewPassword = document.querySelector("#previewPassword");
+const previewAddress = document.querySelector("#previewAddress");
+const previewState = document.querySelector("#previewState");
+const previePhoneNumber = document.querySelector("#previewPhoneNumber");
+const previewTollFreeNumber = document.querySelector("#previewTollFreeNumber");
+const previewEmailid = document.querySelector("#previewEmailid");
+const previewCompanyName = document.querySelector("#previewCompanyName");
+const previewOfficeSpace = document.querySelector("#previewOfficeSpace");
+const previewWebsite = document.querySelector("#previewWebsite");
+
+// Main Buttons
+const disabledBtn1 = document.getElementById("disabledBtn1");
+
+// Arrow function
+const handleFormDatas = () => {
+  previewSection.classList.remove("hidden");
+
+  const {
+    agencyName,
+    agencyType,
+    password,
+    address,
+    state,
+    emailId,
+    radio,
+    telephoneNum,
+    tollFreeNum,
+    companyName,
+    website,
+  } = formEl.elements;
+
+  previewAgencyName.textContent = agencyName.value;
+  previewAgencyType.textContent = agencyType.value;
+  previewPassword.textContent = password.value;
+  previewAddress.textContent = address.value;
+  previewState.textContent = state.value;
+  previePhoneNumber.textContent = telephoneNum.value;
+  previewTollFreeNumber.textContent = tollFreeNum.value;
+  previewEmailid.textContent = emailId.value;
+  previewOfficeSpace.textContent = radio.value;
+  previewCompanyName.textContent = companyName.value;
+  previewWebsite.textContent = website.value;
+};
+
+// if success
+validator.onSuccess(handleFormDatas);
+
+const previewCancelBtn = document.getElementById("previewCancelBtn");
+const previewConfirmBtn = document.getElementById("previewConfirmBtn");
+const alertBox = document.getElementById("alertBox");
+
+previewCancelBtn.addEventListener("click", () => {
+  previewSection.classList.add("hidden");
 });
+
+const confirmEvent = () => {
+  disabledBtn1.disabled = true;
+
+  const [
+    agencyName,
+    agencyType,
+    password,
+    address,
+    state,
+    emailId,
+    radio,
+    telephoneNum,
+    tollFreeNum,
+    companyName,
+    website,
+  ] = formEl.elements;
+
+  previewSection.classList.add("hidden");
+  alertBox.classList.remove("hidden");
+
+  setTimeout(() => {
+    alertBox.classList.add("hidden");
+
+    // reseting all Values from inputs
+    agencyName.value = "";
+    agencyType.value = "";
+    password.value = "";
+    address.value = "";
+    state.value = "";
+    emailId.value = "";
+    radio.value = "";
+    telephoneNum.value = "";
+    companyName.value = "";
+    tollFreeNum.value = "";
+    website.value = "";
+
+    disabledBtn1.disabled = false;
+  }, 7000);
+};
+previewConfirmBtn.addEventListener("click", confirmEvent);
